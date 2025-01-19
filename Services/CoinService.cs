4e314949f94c64c6a8e284365013a24345ba52fa
Leonardo Kiszewski <kiszewski1999@gmail.com>
2025-01-19 18:32:30 -0300

    Create service

---
+++ Services/CoinService.cs
@@ -0,0 +1,25 @@
+class CoinService
+{
+    private CoinRepository _repository;
+
+    public CoinService(CoinRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task PopulateCoins()
+    {
+        var remoteRecords = await _repository.GetRemoteCoinRecords();
+
+        if (remoteRecords.Any())
+        {
+            var coins = remoteRecords.Select(x => x.GeckoCoinToCoin());
+            await _repository.AddCoinRecords(coins);
+        }
+    }
+
+    public async Task AnalyzeCoins()
+    {
+        await _repository.GetLocalCoinRecords();
+    }
+}
