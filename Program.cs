90cb5ff22d41cac06f56550551ed08c7f0347e01
Leonardo Kiszewski <kiszewski1999@gmail.com>
2025-01-25 13:46:39 -0300

    Init mongo db

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
+
+BsonSerializer.RegisterSerializer(new GuidSerializer(GuidRepresentation.Standard));
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSingleton<IMongoClient>(e =>
+{
+    var connectionUri = Env.GetString("MONGO_DB");
+    var settings = MongoClientSettings.FromConnectionString(connectionUri);
+
+    return new MongoClient(settings);
+});
 builder.Services.AddHttpClient<CoinRepository>();
 builder.Services.AddSingleton<CoinService>();
 
