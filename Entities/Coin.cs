1198ce7415a9ce60a6246934300fc59c33cce55d
Leonardo Kiszewski <kiszewski1999@gmail.com>
2025-01-25 14:39:30 -0300

    Change coin to have a list of snapshots

--- Entities/Coin.cs
+++ Entities/Coin.cs
@@ -3,6 +3,12 @@ public class Coin
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Code { get; set; }
+    public IEnumerable<CoinDataSnapshot> Snapshots { get; set; }
+}
+
+public class CoinDataSnapshot
+{
+    public DateTime date { get; set; }
     public decimal DolarPrice { get; set; }
     public long DolarMarketCap { get; set; }
     public decimal Volume { get; set; }
--- Tools/CoinMapper.cs
+++ Tools/CoinMapper.cs
@@ -2,13 +2,19 @@ public static class CoinMapper
 {
     public static Coin GeckoCoinToCoin(this GeckoCoinDto coinDto)
     {
+        CoinDataSnapshot snapshot = new CoinDataSnapshot
+        {
+            date = DateTime.Now,
+            DolarMarketCap = coinDto.MarketCap,
+            DolarPrice = coinDto.CurrentPrice,
+            Volume = coinDto.TotalVolume,
+        };
+
         return new Coin
         {
             Code = coinDto.Id,
             Name = coinDto.Name,
-            DolarMarketCap = coinDto.MarketCap,
-            DolarPrice = coinDto.CurrentPrice,
-            Volume = coinDto.TotalVolume,
+            Snapshots = new List<CoinDataSnapshot> { snapshot }
         };
     }
 }
